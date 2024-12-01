605ff0e9ab94720232db5ee7a2d63cc3f1e82207
sabasm <smma1992@gmail.com>
2024-11-30 17:29:07 -0700

    fix

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,4 @@
-export const getEnvironmentState = () => ({
+export const getEnvironmentState = (): { production: boolean; development: boolean } => ({
     production: process.env.NODE_ENV === 'production',
     development: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 });
