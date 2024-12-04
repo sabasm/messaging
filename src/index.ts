1f8d74f416c39f2e08fe4e1c5bc4391b6cb1a73e
sabasm <smma1992@gmail.com>
2024-12-03 19:57:37 -0700

    bumped version to 1.2.5

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+export * from './MessagingContext';
+export * from './types/message.types';
+export * from './core/types/metrics';
 export * from './infrastructure/amqp/interfaces';
 export * from './infrastructure/amqp/connection';
 export * from './infrastructure/amqp/ConnectionManager';
-
-
